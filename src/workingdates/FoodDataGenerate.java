d5aaa75fb180c27f1b79654a7361c70de4f34df5
Arif EROL <arif.erol16@gmail.com>
2020-03-09 11:42:08 +0300

    test commit

--- src/workingdates/FoodDataGenerate.java
+++ src/workingdates/FoodDataGenerate.java
@@ -82,69 +82,37 @@ public class FoodDataGenerate {
     private static List<FoodModel> getSampleFoodList() throws ParseException {
         List<FoodModel> foodList = new ArrayList<FoodModel>() {
             {
-                /*subathafta4-start*/
-                add(new FoodModel(dfPattern.parse("24.02.2020"), 1.1f, "Mercimek Çorba"));
-                add(new FoodModel(dfPattern.parse("24.02.2020"), 1.2f, "Dana Rosto"));
-                add(new FoodModel(dfPattern.parse("24.02.2020"), 1.3f, "Nohutlu Pirinç Pilavı"));
-                add(new FoodModel(dfPattern.parse("24.02.2020"), 1.4f, "Yoğurtlu Kereviz Salatası"));
-                add(new FoodModel(dfPattern.parse("24.02.2020"), 1.5f, "Meyve"));
+                /*haftalik-start*/
+                add(new FoodModel(dfPattern.parse("09.03.2020"), 1.1f, "Mercimek Çorba"));
+                add(new FoodModel(dfPattern.parse("09.03.2020"), 1.2f, "Etli Kuru Fasulye"));
+                add(new FoodModel(dfPattern.parse("09.03.2020"), 1.3f, "Pirinç Pilavı"));
+                add(new FoodModel(dfPattern.parse("09.03.2020"), 1.4f, "Turşu"));
+                add(new FoodModel(dfPattern.parse("09.03.2020"), 1.5f, "Laz Böreği"));                
                 
-                add(new FoodModel(dfPattern.parse("25.02.2020"), 1.4f, "Yarma Çorba"));
-                add(new FoodModel(dfPattern.parse("25.02.2020"), 1.5f, "Tavuk Sote"));
-                add(new FoodModel(dfPattern.parse("25.02.2020"), 1.6f, "Bulgur Pilavı"));
-                add(new FoodModel(dfPattern.parse("25.02.2020"), 1.7f, "Ayran"));
-                add(new FoodModel(dfPattern.parse("25.02.2020"), 1.8f, "Ayva Tatlısı"));
-                
-                add(new FoodModel(dfPattern.parse("26.02.2020"), 1.9f, "Tarhana Çorba"));
-                add(new FoodModel(dfPattern.parse("26.02.2020"), 1.10f, "Yoğurtlu Köfte"));
-                add(new FoodModel(dfPattern.parse("26.02.2020"), 1.11f, "Şakşuka"));
-                add(new FoodModel(dfPattern.parse("26.02.2020"), 1.12f, "Meyve"));
-                add(new FoodModel(dfPattern.parse("26.02.2020"), 1.13f, "Seçmeli İçecek"));
-                
-                add(new FoodModel(dfPattern.parse("27.02.2020"), 1.14f, "Ezogelin Çorba"));
-                add(new FoodModel(dfPattern.parse("27.02.2020"), 1.15f, "Orman Kebap"));
-                add(new FoodModel(dfPattern.parse("27.02.2020"), 1.16f, "Kol Böreği"));
-                add(new FoodModel(dfPattern.parse("27.02.2020"), 1.17f, "Komposto"));
-                add(new FoodModel(dfPattern.parse("27.02.2020"), 1.18f, "Yoğurt"));
-                
-                add(new FoodModel(dfPattern.parse("28.02.2020"), 1.19f, "Brokoli Çorba"));
-                add(new FoodModel(dfPattern.parse("28.02.2020"), 1.20f, "Et Döner-Garnitür"));
-                add(new FoodModel(dfPattern.parse("28.02.2020"), 1.21f, "Pirinç Pilavı"));
-                add(new FoodModel(dfPattern.parse("28.02.2020"), 1.22f, "Ayran"));
-                add(new FoodModel(dfPattern.parse("28.02.2020"), 1.23f, "Şöbiyet"));
-                /*subathafta4-end*/
-//////////////////////////////////////////////////////////                                
-                /*marthafta1-start*/
-                add(new FoodModel(dfPattern.parse("02.03.2020"), 1.1f, "Köylü Çorba"));
-                add(new FoodModel(dfPattern.parse("02.03.2020"), 1.2f, "Patlıcan Kebap"));
-                add(new FoodModel(dfPattern.parse("02.03.2020"), 1.3f, "Pirinç Pilavı"));
-                add(new FoodModel(dfPattern.parse("02.03.2020"), 1.4f, "Cacık"));
-                add(new FoodModel(dfPattern.parse("02.03.2020"), 1.5f, "Panna Cotta"));                
-                
-                add(new FoodModel(dfPattern.parse("03.03.2020"), 1.1f, "Mercimek Çorba"));
-                add(new FoodModel(dfPattern.parse("03.03.2020"), 1.2f, "Tepsi Köfte"));
-                add(new FoodModel(dfPattern.parse("03.03.2020"), 1.3f, "Lor Peynirli Makarna"));
-                add(new FoodModel(dfPattern.parse("03.03.2020"), 1.4f, "Yoğurtlu Kereviz Salatası"));
-                add(new FoodModel(dfPattern.parse("03.03.2020"), 1.5f, "Meyve suyu"));                
+                add(new FoodModel(dfPattern.parse("10.03.2020"), 1.1f, "Şehriye Çorba"));
+                add(new FoodModel(dfPattern.parse("10.03.2020"), 1.2f, "Mantarlı Tavuk Biftek"));
+                add(new FoodModel(dfPattern.parse("10.03.2020"), 1.3f, "Makarna Salata"));
+                add(new FoodModel(dfPattern.parse("10.03.2020"), 1.4f, "Ayran"));
+                add(new FoodModel(dfPattern.parse("10.03.2020"), 1.5f, "Meyve"));                
 
-                add(new FoodModel(dfPattern.parse("04.03.2020"), 1.1f, "Şehriye Çorba"));
-                add(new FoodModel(dfPattern.parse("04.03.2020"), 1.2f, "Fırın Tavuk"));
-                add(new FoodModel(dfPattern.parse("04.03.2020"), 1.3f, "Alaca Pilav"));
-                add(new FoodModel(dfPattern.parse("04.03.2020"), 1.4f, "Seçmeli İçecek"));
-                add(new FoodModel(dfPattern.parse("04.03.2020"), 1.5f, "Fıstıklı Kadayıf"));                
+                add(new FoodModel(dfPattern.parse("11.03.2020"), 1.1f, "Tavuk Suyu Çorba"));
+                add(new FoodModel(dfPattern.parse("11.03.2020"), 1.2f, "Yoğurtlu Köfte"));
+                add(new FoodModel(dfPattern.parse("11.03.2020"), 1.3f, "Barbunya Pilaki"));
+                add(new FoodModel(dfPattern.parse("11.03.2020"), 1.4f, "Seçmeli İçecek"));
+                add(new FoodModel(dfPattern.parse("11.03.2020"), 1.5f, "Trileçe/Sütlaç"));                
 
-                add(new FoodModel(dfPattern.parse("05.03.2020"), 1.1f, "Tarhana Çorba"));
-                add(new FoodModel(dfPattern.parse("05.03.2020"), 1.2f, "Tas Kebap"));
-                add(new FoodModel(dfPattern.parse("05.03.2020"), 1.3f, "Bulgur Pilavı"));
-                add(new FoodModel(dfPattern.parse("05.03.2020"), 1.4f, "Kırmızı Lahana - Havuç Salata"));
-                add(new FoodModel(dfPattern.parse("05.03.2020"), 1.5f, "Yoğurt"));                
+                add(new FoodModel(dfPattern.parse("12.03.2020"), 1.1f, "Yeşil Mercimek Çorba"));
+                add(new FoodModel(dfPattern.parse("12.03.2020"), 1.2f, "Orman Kebabı"));
+                add(new FoodModel(dfPattern.parse("12.03.2020"), 1.3f, "Peynirli Su Böreği"));
+                add(new FoodModel(dfPattern.parse("12.03.2020"), 1.4f, "Pembe Sultan"));
+                add(new FoodModel(dfPattern.parse("12.03.2020"), 1.5f, "Komposto"));                
 
-                add(new FoodModel(dfPattern.parse("06.03.2020"), 1.1f, "Ezogelin Çorba"));
-                add(new FoodModel(dfPattern.parse("06.03.2020"), 1.2f, "Arnavut Ciğeri-Garnitür"));
-                add(new FoodModel(dfPattern.parse("06.03.2020"), 1.3f, "Pirinç Pilavı"));
-                add(new FoodModel(dfPattern.parse("06.03.2020"), 1.4f, "Ayran"));
-                add(new FoodModel(dfPattern.parse("06.03.2020"), 1.5f, "Tiramisu"));
-                /*marthafta1-end*/
+                add(new FoodModel(dfPattern.parse("13.03.2020"), 1.1f, "Ezogelin Çorba"));
+                add(new FoodModel(dfPattern.parse("13.03.2020"), 1.2f, "Et Döner-Garnitür"));
+                add(new FoodModel(dfPattern.parse("13.03.2020"), 1.3f, "Pirinç Pilavı"));
+                add(new FoodModel(dfPattern.parse("13.03.2020"), 1.4f, "Ayran"));
+                add(new FoodModel(dfPattern.parse("13.03.2020"), 1.5f, "Sakızlı Muhallebi/Revani"));
+                /*haftalik-end*/
                 //////////////////////////////////////////////////////////
                 
             }
